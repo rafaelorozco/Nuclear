1cab40bb22835701db73a59ad537512c2fe5afac
Rafael Orozco <rao9787@gmail.com>
2016-10-30 20:10:12 -0700

    Update globe.js

--- globe.js
+++ globe.js
@@ -99,7 +99,7 @@ DAT.Globe = function(container, opts) {
 
     scene = new THREE.Scene();
 
-    var geometry = new THREE.SphereGeometry(200, 1, 1);
+    var geometry = new THREE.SphereGeometry(200, 64, 64);
 
     shader = Shaders['earth'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
