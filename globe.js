5be6a22912de2479d80815e319f179373a1f24cc
Rafael Orozco <rao9787@gmail.com>
2016-10-30 19:39:12 -0700

    Update globe.js

--- globe.js
+++ globe.js
@@ -18,7 +18,7 @@ DAT.Globe = function(container, opts) {
   
   var colorFn = opts.colorFn || function(x) {
     var c = new THREE.Color();
-    c.setHSL( ( 0.6 - ( 2.5 * 0.5 ) ), 1.0, 0.5 );
+    c.setHSL( ( 0.6 - ( 0.5 * 0.5 ) ), 1.0, 0.5 );
     return c;
   };
   var imgDir = opts.imgDir || '/globe/';
